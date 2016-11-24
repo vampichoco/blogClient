bdd47b60cf64a4d1b8bd82e2d264ecbde703d7c0
vampichoco <eskimo@sadzee.com>
2016-11-24 15:04:02 -0600

    first version
    ajax

---
+++ app/blog.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    onLoad();
+});
+
+function onLoad(){
+    loadBlog();
+    $('#showform-create').click(function(){
+        $('#addEntryModal').modal('show');
+    }) 
+    
+    $('#sendEntryButton').click(sendEntry);
+}
+
+function loadBlog(){
+    var url = "http://localhost:5000/blog"
+    $.getJSON(url, function(res){
+       $.each(res, function(index){
+           var entry = res[index];
+           var title = $('<h3></h3>').html(entry.title); 
+           var text = $('<article></article>').html(entry.text);
+           
+           var div = $('<div class="entry"></div>') 
+           div.append(title); 
+           div.append(text); 
+           
+            $('#blogArea').append(div);
+           
+       }) 
+    });
+}
+
+function sendEntry(){
+    var title = $('#titletb').val(); 
+    var text = $('#texttb').val(); 
+    
+    var ob = {title: title, text:text}
+    var data = JSON.stringify(ob);
+    
+    var url = "http://localhost:5000/upload";
+
+           $.ajax({
+               type: "POST",
+               data: data,
+               url: url,
+               contentType: "application/json",
+               dataType: 'json'
+           }).done(function (res) {
+               var titlehtml = $('<h3></h3>').html(title); 
+               var texthtml = $('<article></article>').html(text);
+           
+               var div = $('<div class="entry"></div>') 
+               div.append(titlehtml); 
+               div.append(texthtml); 
+           
+               $('#blogArea').append(div);
+           }).fail(function (err) {
+               
+           })
+           
+     $('#addEntryModal').modal('hide');
+}
+ 
